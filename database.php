cdaab4aa14771cdf358a4fde5c385b6708cbbdf3
Filippo Sbaiz <sbaizfilippo@gmail.com>
2023-02-06 16:29:52 +0100

    font awsome icon

--- database.php
+++ database.php
@@ -5,18 +5,18 @@
     include __DIR__ . '/models/altro.php';
 
     // CIBO
-    $cibo_1 = new Cibo('https://picsum.photos/200', 'Royal Canin Mini Adult', 'Cane', '$43,99', '545g', 'prosciutto, riso');
-    $cibo_2 = new Cibo('https://picsum.photos/200', 'Almo Nature Holistic Maintenance Medium Large Tonno e Riso', 'Cane', '$44,99', '600g', 'manzo, cereali');
-    $cibo_3 = new Cibo('https://picsum.photos/200', 'Almo Nature Cat Daily Lattina', 'Gatto', '$34,99', '400g', 'tonno, pollo, prosciutto');
-    $cibo_4 = new Cibo('https://picsum.photos/200', 'Mangime per Pesci Guppy in Fiocchi', 'Pesce', '$2,95', '30g', 'Pesci e sottoprodotti dei pesci, Cereali, Lieviti, Alghe');
+    $cibo_1 = new Cibo('https://picsum.photos/200', 'Royal Canin Mini Adult', '<i class="fa-solid fa-dog"></i> Cane', '$43,99', '545g', 'prosciutto, riso');
+    $cibo_2 = new Cibo('https://picsum.photos/200', 'Almo Nature Holistic Maintenance Medium Large Tonno e Riso', '<i class="fa-solid fa-dog"></i> Cane', '$44,99', '600g', 'manzo, cereali');
+    $cibo_3 = new Cibo('https://picsum.photos/200', 'Almo Nature Cat Daily Lattina', ' <i class="fa-solid fa-cat"></i>Gatto', '$34,99', '400g', 'tonno, pollo, prosciutto');
+    $cibo_4 = new Cibo('https://picsum.photos/200', 'Mangime per Pesci Guppy in Fiocchi', '<i class="fa-solid fa-fish"></i> Pesce', '$2,95', '30g', 'Pesci e sottoprodotti dei pesci, Cereali, Lieviti, Alghe');
 
     // ALTRO
-    $altro_1 = new Altro('https://picsum.photos/200', 'Voliera Wilma in Legno', 'Uccello', '$184,99', 'M: L 83 x P 67 x H 153 cm', 'Legno');
-    $altro_2 = new Altro('https://picsum.photos/200', '', 'Pesce', '$2,29', 'ND', 'Materiale Espanso');
+    $altro_1 = new Altro('https://picsum.photos/200', 'Voliera Wilma in Legno', '<i class="fa-solid fa-dove"></i> Uccello', '$184,99', 'M: L 83 x P 67 x H 153 cm', 'Legno');
+    $altro_2 = new Altro('https://picsum.photos/200', 'Cartucce Filtranti per Filtro EasyCrystal', '<i class="fa-solid fa-fish"></i> Pesce', '$2,29', 'ND', 'Materiale Espanso');
 
     // GIOCHI
-    $giochi_1 = new Giochi('https://picsum.photos/200', 'Kong Classic', 'Cane', '$13,49', '8,5cm x 10cm', 'Galleggia e Rimbalza');
-    $giochi_2 = new Giochi('https://picsum.photos/200', 'Topini di Peluche Trixie', 'Gatto', '$4,99', '8,5cm x 10cm', 'Morbido con codina in corda');
+    $giochi_1 = new Giochi('https://picsum.photos/200', 'Kong Classic', '<i class="fa-solid fa-dog"></i> Cane', '$13,49', '8,5cm x 10cm', 'Galleggia e Rimbalza');
+    $giochi_2 = new Giochi('https://picsum.photos/200', 'Topini di Peluche Trixie', '<i class="fa-solid fa-cat"></i> Gatto', '$4,99', '8,5cm x 10cm', 'Morbido con codina in corda');
 
     // ARRAY CIBO
     $arrayCibo = [$cibo_1, $cibo_2, $cibo_3, $cibo_4];
--- index.php
+++ index.php
@@ -17,6 +17,7 @@
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.3.0/axios.min.js" integrity="sha512-A6BG70odTHAJYENyMrDN6Rq+Zezdk+dFiFFN6jH1sB+uJT3SYMV4zDSVR+7VawJzvq7/IrT/2K3YWVKRqOyN0Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>php oop 2</title>
 </head>
@@ -25,10 +26,20 @@
     <div id="app">
 
         <div class="container">
-            <div class="row">
+        <div class="row text-center">
+                <div class="col-12">
+                    <h1>Boolshop</h1>
+                </div>
+            </div>
+            <div class="row text-center">
+                <div class="col-12">
+                    <h2>I nostri prodotti</h2>
+                </div>
+            </div>
+            <div class="row justify-content-center">
                 <!-- CIBO -->
                 <?php foreach($arrayCibo as $cibo) { ?>
-                    <div class="card" style="width: 15rem;">
+                    <div class="card p-2 m-2" style="width: 20rem;">
                         <img class="card-img-top" src="<?php echo $cibo->immagine ?>" alt="card img">
                         <div class="card-body">
                             <h3><?php echo $cibo->nome ?></h3>
@@ -49,7 +60,7 @@
                 <?php } ?>
                 <!-- ALTRO -->
                 <?php foreach($arrayAltro as $altro) { ?>
-                    <div class="card" style="width: 15rem;">
+                    <div class="card p-2 m-2" style="width: 20rem;">
                         <img class="card-img-top" src="<?php echo $altro->immagine ?>" alt="card img">
                         <div class="card-body">
                             <h3><?php echo $altro->nome ?></h3>
@@ -70,7 +81,7 @@
                 <?php } ?>
                 <!-- GIOCHI -->
                 <?php foreach($arrayGiochi as $giochi) { ?>
-                    <div class="card" style="width: 15rem;">
+                    <div class="card p-2 m-2" style="width: 20rem;">
                         <img class="card-img-top" src="<?php echo $giochi->immagine ?>" alt="card img">
                         <div class="card-body">
                             <h3><?php echo $giochi->nome ?></h3>
