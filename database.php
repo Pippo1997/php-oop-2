3143aa1e019fbbe7fa95124217310b6b6ea143ef
Filippo Sbaiz <sbaizfilippo@gmail.com>
2023-02-06 14:25:51 +0100

    first commit

---
+++ database.php
@@ -0,0 +1,7 @@
+<?php
+    include __DIR__ . '/models/prodotti.php';
+    include __DIR__ . '/models/cani.php';
+    include __DIR__ . '/models/gatti.php';
+    include __DIR__ . '/models/pesci.php';
+    include __DIR__ . '/models/uccelli.php';
+?>
