7e849d8cb87ed2faf6186fe7606b462bb0bcbd72
Filippo Sbaiz <sbaizfilippo@gmail.com>
2023-02-06 15:40:39 +0100

    img picsum

--- database.php
+++ database.php
@@ -7,18 +7,18 @@
     echo "<pre>";
 
     // CIBO
-    $cibo_1 = new Cibo('immagine', 'Royal Canin Mini Adult', 'Cane', '$43,99', '545g', 'prosciutto, riso');
-    $cibo_2 = new Cibo('immagine', 'Almo Nature Holistic Maintenance Medium Large Tonno e Riso', 'Cane', '$44,99', '600g', 'manzo, cereali');
-    $cibo_3 = new Cibo('immagine', 'Almo Nature Cat Daily Lattina', 'Gatto', '$34,99', '400g', 'tonno, pollo, prosciutto');
-    $cibo_4 = new Cibo('immagine', 'Mangime per Pesci Guppy in Fiocchi', 'Pesce', '$2,95', '30g', 'Pesci e sottoprodotti dei pesci, Cereali, Lieviti, Alghe');
+    $cibo_1 = new Cibo('https://picsum.photos/200', 'Royal Canin Mini Adult', 'Cane', '$43,99', '545g', 'prosciutto, riso');
+    $cibo_2 = new Cibo('https://picsum.photos/200', 'Almo Nature Holistic Maintenance Medium Large Tonno e Riso', 'Cane', '$44,99', '600g', 'manzo, cereali');
+    $cibo_3 = new Cibo('https://picsum.photos/200', 'Almo Nature Cat Daily Lattina', 'Gatto', '$34,99', '400g', 'tonno, pollo, prosciutto');
+    $cibo_4 = new Cibo('https://picsum.photos/200', 'Mangime per Pesci Guppy in Fiocchi', 'Pesce', '$2,95', '30g', 'Pesci e sottoprodotti dei pesci, Cereali, Lieviti, Alghe');
 
     // ALTRO
-    $altro_1 = new Altro('immagine', 'Voliera Wilma in Legno', 'Uccello', '$184,99', 'M: L 83 x P 67 x H 153 cm', 'Legno');
-    $altro_2 = new Altro('immagine', '', 'Pesce', '$2,29', 'ND', 'Materiale Espanso');
+    $altro_1 = new Altro('https://picsum.photos/200', 'Voliera Wilma in Legno', 'Uccello', '$184,99', 'M: L 83 x P 67 x H 153 cm', 'Legno');
+    $altro_2 = new Altro('https://picsum.photos/200', '', 'Pesce', '$2,29', 'ND', 'Materiale Espanso');
 
     // GIOCHI
-    $giochi_1 = new Giochi('immagine', 'Kong Classic', 'Cane', '$13,49', '8,5cm x 10cm', 'Galleggia e Rimbalza');
-    $giochi_2 = new Giochi('immagine', 'Topini di Peluche Trixie', 'Gatto', '$4,99', '8,5cm x 10cm', 'Morbido con codina in corda');
+    $giochi_1 = new Giochi('https://picsum.photos/200', 'Kong Classic', 'Cane', '$13,49', '8,5cm x 10cm', 'Galleggia e Rimbalza');
+    $giochi_2 = new Giochi('https://picsum.photos/200', 'Topini di Peluche Trixie', 'Gatto', '$4,99', '8,5cm x 10cm', 'Morbido con codina in corda');
 
     var_dump($cibo_1);
 
