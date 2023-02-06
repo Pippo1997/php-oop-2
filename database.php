873418992bf913760fcb4e2735d5038c7cbd373e
Filippo Sbaiz <sbaizfilippo@gmail.com>
2023-02-06 15:49:23 +0100

    creazione array

--- database.php
+++ database.php
@@ -4,8 +4,6 @@
     include __DIR__ . '/models/giochi.php';
     include __DIR__ . '/models/altro.php';
 
-    echo "<pre>";
-
     // CIBO
     $cibo_1 = new Cibo('https://picsum.photos/200', 'Royal Canin Mini Adult', 'Cane', '$43,99', '545g', 'prosciutto, riso');
     $cibo_2 = new Cibo('https://picsum.photos/200', 'Almo Nature Holistic Maintenance Medium Large Tonno e Riso', 'Cane', '$44,99', '600g', 'manzo, cereali');
@@ -20,7 +18,16 @@
     $giochi_1 = new Giochi('https://picsum.photos/200', 'Kong Classic', 'Cane', '$13,49', '8,5cm x 10cm', 'Galleggia e Rimbalza');
     $giochi_2 = new Giochi('https://picsum.photos/200', 'Topini di Peluche Trixie', 'Gatto', '$4,99', '8,5cm x 10cm', 'Morbido con codina in corda');
 
-    var_dump($cibo_1);
+    // ARRAY CIBO
+    $arrayCibo = [$cibo_1, $cibo_2, $cibo_3, $cibo_4];
 
-    echo "</pre>";
+    // ARRAY ALTRO
+    $arrayAltro = [$altro_1, $altro_2];
+
+    // ARRAY GIOCHI
+    $arrayGiochi = [$giochi_1, $giochi_2];
+
+
+
+
 ?>
